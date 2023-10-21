414af7ea840e903f4aaf328e352e424f88fe6bcb
Puchakayala Satwika <psatwika@virtusa.com>
2023-10-21 17:33:41 +0530

    Added quickseach componnet, added jquery, changes in teaser component for list, changes for search suggetions

--- aemavengers/core/src/main/java/com/aem/avengers/core/servlets/ArticleSearchServlet.java
+++ aemavengers/core/src/main/java/com/aem/avengers/core/servlets/ArticleSearchServlet.java
@@ -1,6 +1,8 @@
 package com.aem.avengers.core.servlets;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
@@ -50,82 +52,132 @@ public class ArticleSearchServlet extends SlingAllMethodsServlet {
 		Session session = resourceResolver.adaptTo(Session.class);
 		Workspace workspace = session.getWorkspace();
 		String searchKeyword = request.getParameter("searchKeyword");
-		String typeKeyword = request.getParameter("typeKeyword");
-		String nodePath = request.getParameter("listComponentNode");
+		String typeKeyword = request.getParameter("fulltext");
 		String currentHit = request.getParameter("currentHit");
 
 		try {
 			QueryManager queryManager = workspace.getQueryManager();
-			Resource list = resourceResolver.getResource(nodePath);
-			Node listNode = list.adaptTo(Node.class);
-			LOGGER.info("listNode :: {}", listNode);
+			String nodePath = "";
+			String pagePath = "";
 			String maxItems = "";
-			if (listNode.hasProperty("maxItems")) {
-				maxItems = listNode.getProperty("maxItems").getString();
-			}
-			String parentPage = "";
-			if (listNode.hasProperty("parentPage")) {
-				parentPage = listNode.getProperty("parentPage").getString();
+			int keyWordOffSet = 0;
+			if (StringUtils.isNotBlank(typeKeyword)) {
+				nodePath = request.getRequestPathInfo().getSuffix();
+				String resultsOffset = request.getParameter("resultsOffset");
+				if (StringUtils.isNotBlank(resultsOffset)) {
+					keyWordOffSet = Integer.valueOf(resultsOffset);
+				}
+				Resource searchRes = resourceResolver.getResource(nodePath);
+				Node searchNode = searchRes.adaptTo(Node.class);
+				LOGGER.info("searchNode :: {}", searchNode);
+				if (searchNode.hasProperty("searchRoot")) {
+					pagePath = searchNode.getProperty("searchRoot").getValue().getString();
+				}
+			} else {
+				nodePath = request.getParameter("listComponentNode");
+				Resource listRes = resourceResolver.getResource(nodePath);
+				Node listNode = listRes.adaptTo(Node.class);
+				LOGGER.info("listNode :: {}", listNode);
+				if (listNode.hasProperty("maxItems")) {
+					maxItems = listNode.getProperty("maxItems").getValue().getString();
+				}
+				if (listNode.hasProperty("parentPage")) {
+					pagePath = listNode.getProperty("parentPage").getValue().getString();
+				}
 			}
 
-			int hit = Integer.valueOf(currentHit);
-			int maxItemsInt = 0;
-			if (StringUtils.isNotBlank(maxItems)) {
-				maxItemsInt = Integer.valueOf(maxItems);
-			}
-//			int offset = (hit) * maxItemsInt;
-//			int endValue = offset + maxItemsInt;
-
-			int limit = maxItemsInt * hit;
-			int offset = limit - maxItemsInt;
-
-			LOGGER.info("limit :: {}", limit);
-			LOGGER.info("offset :: {}", offset);
-			String queryString = "/jcr:root".concat(parentPage).concat("//element(*, cq:Page)");
+			String queryString = "/jcr:root".concat(pagePath).concat("//element(*, cq:Page)");
+			LOGGER.info("queryString :: {}", queryString);
 			Query query = queryManager.createQuery(queryString, "xpath");
+			List<String> keyWordList = new LinkedList<String>();
+			if (StringUtils.isNotBlank(searchKeyword)) {
+				Integer hit = 0;
+				if (StringUtils.isNotBlank(currentHit)) {
+					hit = Integer.valueOf(currentHit);
+				}
+				int maxItemsInt = 0;
+				if (StringUtils.isNotBlank(maxItems)) {
+					maxItemsInt = Integer.valueOf(maxItems);
+				}
+//				int offset = (hit) * maxItemsInt;
+//				int endValue = offset + maxItemsInt;
+
+				int limit = maxItemsInt * hit;
+				int offset = limit - maxItemsInt;
+
+				LOGGER.info("limit :: {}", limit);
+				LOGGER.info("offset :: {}", offset);
+				query.setLimit(limit);
+				query.setOffset(offset);
+			}
 			QueryResult result = query.execute();
 			NodeIterator nodes = result.getNodes();
 			String contentStr = "";
 			JsonArray jsonArr = new JsonArray();
 			while (nodes.hasNext()) {
 				Node node = nodes.nextNode();
-				JsonObject jsonObj = new JsonObject();
 				Page page = pageManager.getPage(node.getPath());
 				String tageName = "";
 				if (page != null) {
 					Tag[] cqTags = page.getTags();
 					for (Tag tag : cqTags) {
 						String tagID = tag.getTagID();
-						tageName = tag.getName();
-						contentStr = contentStr.concat(tagID + " ");
+						tageName = tag.getName().toLowerCase();
+						contentStr = contentStr + tagID + " ";
 					}
 				}
-				String pageTitle = page.getTitle();
-				contentStr = contentStr.concat(pageTitle);
+				String pageTitle = page.getTitle().toLowerCase();
+				contentStr = contentStr + pageTitle;
 				ValueMap pageProperties = page.getProperties();
 				String author = "";
 				if (pageProperties.containsKey("author")) {
-					author = pageProperties.get("author", String.class);
-					contentStr = contentStr.concat(author);
+					author = pageProperties.get("author", String.class).toLowerCase();
+					contentStr = contentStr + author;
 				}
+				LOGGER.info("contentStr :: {}", contentStr);
 				if (StringUtils.isNotBlank(searchKeyword) && StringUtils.isNotBlank(contentStr)
 						&& StringUtils.isNotBlank(searchKeyword)
 						&& StringUtils.containsIgnoreCase(contentStr, searchKeyword)) {
 					jsonArr.add(avengersBasicService.addPageProperties(page));
 				} else if (StringUtils.isNotBlank(typeKeyword)) {
 					if (StringUtils.isNotBlank(author) && author.startsWith(typeKeyword)) {
-						jsonArr.add(author.toLowerCase());
+						if (!keyWordList.contains(author)) {
+							keyWordList.add(author);
+						}
+//						jsonArr.add(author.toLowerCase());
 					}
 					if (StringUtils.isNotBlank(tageName) && tageName.startsWith(typeKeyword)) {
-						jsonArr.add(tageName.toLowerCase());
+						if (!keyWordList.contains(tageName)) {
+							keyWordList.add(tageName);
+						}
+//						jsonArr.add(tageName.toLowerCase());
 					}
 					if (StringUtils.isNotBlank(pageTitle) && pageTitle.startsWith(typeKeyword)) {
-						jsonArr.add(pageTitle.toLowerCase());
+						if (!keyWordList.contains(pageTitle)) {
+							keyWordList.add(pageTitle);
+						}
+//						jsonArr.add(pageTitle.toLowerCase());
 					}
 
 				}
 			}
-
+			LOGGER.info("keyWordList :: {}", keyWordList);
+			LOGGER.info("keyWordList size :: {}", keyWordList.size());
+			LOGGER.info("keyWordOffSet :: {}", keyWordOffSet);
+			int keyWordsLimit = keyWordOffSet + 10;
+			LOGGER.info("keyWordsLimit :: {}", keyWordsLimit);
+			if (keyWordList.size() < keyWordsLimit) {
+				keyWordsLimit = keyWordList.size();
+			}
+			for (int i = keyWordOffSet; i < keyWordsLimit; i++) {
+				if (keyWordList.size() >= keyWordOffSet) {
+					String keyword = keyWordList.get(i);
+					JsonObject json = new JsonObject();
+					json.addProperty("title", keyword);
+					jsonArr.add(json);
+				}
+			}
+			LOGGER.info("jsonArr :: {}", jsonArr);
 			response.getWriter().write(jsonArr.toString());
 		} catch (RepositoryException e) {
 			LOGGER.error("RepositoryException in doGet :: {}", e);
